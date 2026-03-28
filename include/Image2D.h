6721c8cf6173d7b91406ddfc328ec4e2de6425bc
GuilhermeAlvesTeixeira <guilhermealvesteixeirasilva@gmail.com>
2026-03-28 20:10:13 -0300

    Gerenciamento de memória + métodos acessores de imagem

--- include/Image2D.h
+++ include/Image2D.h
@@ -2,12 +2,76 @@
 #define IMAGE2D_H
 
 #include <iostream>
+#include <exception>
 
 template<typename T>
 
 class Image2D {
+private:
+    T** data;
+    size_t width;
+    size_t height;
+
+public:
+    //Construtores
+    Image2D();
+    Image2D(size_t w, size_t h) : width(w), height(h) {
+        data = new T*[height]; //Aloca array de ponteiros (linhas)
+
+        for(size_t i=0; i < height; i++) {
+            data[i] = new T[width]; // p/cada linha, aloca um array de T colunas.
+        }
+    };
+
+    //Destrutor
+    ~Image2D() {
+        for(size_t i = 0; i < height; i++) {
+            delete[] data[i];
+        }
+        delete[] data;
+        data = nullptr;
+    }
+
+    //Métodos acessores (GET)
+    
+    int getWidth() const {
+        return width;
+    }
+
+    int getHeight() const {
+        return height;
+    }
+
+    //Métodos acessores PIXEL (SET e GET)
+    //Lembrete: col -> largura | row -> altura
+
+    void setPixel(int row, int col , const T& value) {
+        if(col < 0 || col >= width || row < 0 || row >= height) {
+            throw std::out_of_range("Índice fora dos limites Imagem!");
+        }
+        data[row][col] = value;
+    }
+
+    T getPixel(int row, int col) {
+        if(col < 0 || col >= width || row < 0 || row >= height) {
+            throw std::out_of_range("Índice fora dos limites Imagem!");
+        }
+        return data[row][col];
+    }
+
+    //Clear: Preenchimento
+    void clear(const T& value) {
+        for(size_t row = 0; row < height; row++) {
+            for(size_t col = 0; col < width; col++) {
+                data[row][col] = value;
+            }
+        }
+    }
+
+    //Bloqueio de Cópia (Regra dos três: Sugerido pelo prof Gilvan)
+    Image2D(const Image2D& other) = delete;
+    Image2D& operator = (const Image2D& other) = delete;
 
 };
-
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 #include "Image2D.h"
 
 int main(int argc, char** argv) {
- std::cout << "Compilou e Rodou com CMake (de novo)!!" << '\n';
- return 0;
+    Image2D<size_t> img(4,3);
+    
+    img.clear(0);
+
+    img.setPixel(1,1,255);
+    img.setPixel(2,0,128);
+
+    for(size_t row = 0; row < img.getHeight(); row++) {
+        for( size_t col = 0; col < img.getWidth(); col++) {
+            std::cout << img.getPixel(row,col) << " ";
+        }
+        std::cout << '\n';
+    }
+    return 0;
 }
