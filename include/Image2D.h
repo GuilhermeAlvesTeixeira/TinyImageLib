63c8dc32cd0ae88b23846398a103cd52351596bd
GuilhermeAlvesTeixeira <guilhermealvesteixeirasilva@gmail.com>
2026-03-28 19:18:35 -0300

    Adicionando classe Image2D, Configs de Cmake e .gitignore

---
+++ include/Image2D.h
@@ -0,0 +1,13 @@
+#ifndef IMAGE2D_H
+#define IMAGE2D_H
+
+#include <iostream>
+
+template<typename T>
+
+class Image2D {
+
+};
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Image2D.h"
+
+int main(int argc, char** argv) {
+ std::cout << "Compilou e Rodou com CMake (de novo)!!" << '\n';
+ return 0;
+}
