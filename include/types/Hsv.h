76d40115bd747969d851d71d3deb1acc9ffe7676
GuilhermeAlvesTeixeira <guilhermealvesteixeirasilva@gmail.com>
2026-04-16 10:36:21 -0300

    Adicionando novos tipos: HSV e YCbCr

---
+++ include/types/Hsv.h
@@ -0,0 +1,10 @@
+#ifndef HSV_STRUCT_H
+#define HSV_STRUCT_H
+
+// H: [0,360];
+// S,V: [0,1]
+struct HSV {
+    float h, s, v;
+};
+
+#endif
---
+++ include/types/YCbCr.h
@@ -0,0 +1,23 @@
+#ifndef YCBCR_STRUCT_H
+#define YCBCR_STRUCT_H
+
+#include <iostream>
+#include <cstdint>
+
+/* Nota (pq eu não lembrava): YCbCr tenta separar luminãncia (brilho) de crominânica (cor).
+   Y -> brilho (luminância)
+   Cb -> componente azul (quanto a cor se afasta para o azul)
+   Cr -> componente vermelho (quanto a cor se afasta para o vermelho)
+
+   O olho humano é muito mais sensível ao brilho do que à cor.
+*/
+
+//Y: [0,255]
+//CbCr: [-128, 127]
+
+struct YCbCr {
+    float y, cb, cr;
+};
+
+
+#endif
